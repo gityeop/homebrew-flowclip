aabb439c63e6f7e83cfe75879a02ef340f033b62
gityeop <freiheit517@icloud.com>
2026-04-17 11:27:43 +0900

    update: FlowClip v1.0.18

--- Casks/flowclip.rb
+++ Casks/flowclip.rb
@@ -1,6 +1,6 @@
 cask "flowclip" do
-  version "1.0.17"
-  sha256 "5cbd43aa53b9fa2f51211b11a7e60f7c6f4cc63fa1edde288ad8130d2740612e"
+  version "1.0.18"
+  sha256 "718953b0e9e840baa299127f2d0deb6bcc30b736f4c24600d45ab3ac99d9c19c"
 
   url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip.zip"
   name "FlowClip"
